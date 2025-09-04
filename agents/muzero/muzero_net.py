371bed6cc870937c1f6de4fedd8298b8b005e94f
Toren Darby <torendarby@gmail.com>
2025-09-04 14:11:21 -0700

    incorrect wip action generation

--- agents/muzero/muzero_net.py
+++ agents/muzero/muzero_net.py
@@ -7,6 +7,10 @@ import torch.nn.functional as F
 from environments.base import BaseEnvironment, StateType, ActionType
 from models.networks import BaseTokenizingNet
 
+# Don't delete
+# Hidden state vae is a distribution for sampling hidden state tensors from, for stochasticity.
+# A hidden state tensor can be used to predict a variable length list of encoded action tokens with size action_dim. These should be stored with the hidden state tensor.
+
 
 class Vae:
     def __init__(self, mu: torch.Tensor, log_var: torch.Tensor):
@@ -38,6 +42,8 @@ class MuZeroNet(BaseTokenizingNet):
             encoder_layer, num_layers=num_encoder_layers
         )
         self.game_token = nn.Parameter(torch.randn(1, 1, embedding_dim))
+        self.fc_hidden_state_mu = nn.Linear(embedding_dim, embedding_dim)
+        self.fc_hidden_state_log_var = nn.Linear(embedding_dim, embedding_dim)
 
         # get_next_hidden_state (dynamics, g)
         # It takes hidden_state + action_embedding -> next_hidden_state_vae
@@ -48,8 +54,10 @@ class MuZeroNet(BaseTokenizingNet):
             nn.Linear(embedding_dim + action_embedding_dim, dynamics_hidden_dim),
             nn.ReLU(),
         )
-        self.dynamics_mu_head = nn.Linear(dynamics_hidden_dim, embedding_dim)
-        self.dynamics_log_var_head = nn.Linear(dynamics_hidden_dim, embedding_dim)
+        self.fc_next_hidden_state_mu = nn.Linear(dynamics_hidden_dim, embedding_dim)
+        self.fc_next_hidden_state_log_var = nn.Linear(
+            dynamics_hidden_dim, embedding_dim
+        )
 
         # get_policy_and_value (prediction, f)
         self.value_head = nn.Sequential(nn.Linear(embedding_dim, 1), nn.Tanh())
@@ -59,6 +67,26 @@ class MuZeroNet(BaseTokenizingNet):
         self.policy_head = nn.Sequential(
             nn.Linear(policy_input_dim, 64), nn.ReLU(), nn.Linear(64, 1)
         )
+
+        # A head to generate actions from a hidden state, token by token.
+        self.action_decoder_lstm = nn.LSTMCell(embedding_dim, embedding_dim)
+        self.hidden_to_lstm_h = nn.Linear(embedding_dim, embedding_dim)
+        self.hidden_to_lstm_c = nn.Linear(embedding_dim, embedding_dim)
+        self.start_action_token = nn.Parameter(torch.randn(1, embedding_dim))
+
+        # Wrong
+        # You generate encoded action token tensors. You do not try to generate game-rule-friendly action descriptions.
+        # Generate tokens of length action_dim, period.
+        # action_spec = self.network_spec["action_space"]
+        # self.action_component_names = action_spec["components"]
+        # self.action_component_heads = nn.ModuleDict()
+        # for comp_name in self.action_component_names:
+        #     cardinality = self.network_spec["cardinalities"][comp_name]
+        #     # Vocab size: values (cardinality+1) + padding (1) + stop token (1)
+        #     vocab_size = cardinality + 3
+        #     self.action_component_heads[comp_name] = nn.Linear(
+        #         embedding_dim, vocab_size
+        #     )
 
     def get_hidden_state_vae(self, state: StateType) -> Vae:
         """
@@ -72,7 +100,9 @@ class MuZeroNet(BaseTokenizingNet):
 
         transformer_output = self.transformer_encoder(sequence)
         game_token_output = transformer_output[:, 0, :]
-        return game_token_output
+        mu = self.fc_hidden_state_mu(game_token_output)
+        log_var = self.fc_hidden_state_log_var(game_token_output)
+        return Vae(mu, log_var)
 
     def get_next_hidden_state_vae(
         self, hidden_state: torch.Tensor, action: ActionType
@@ -86,15 +116,72 @@ class MuZeroNet(BaseTokenizingNet):
         dynamics_input = torch.cat([hidden_state, action_token], dim=1)
         base_output = self.dynamics_network_base(dynamics_input)
 
-        mu = self.dynamics_mu_head(base_output)
-        log_var = self.dynamics_log_var_head(base_output)
+        mu = self.fc_next_hidden_state_mu(base_output)
+        log_var = self.fc_next_hidden_state_log_var(base_output)
         return Vae(mu, log_var)
 
     def get_actions_for_hidden_state(
-        self, hidden_state: torch.Tensor
+        self,
+        hidden_state: torch.Tensor,
+        num_actions_to_sample: int = 10,
     ) -> List[ActionType]:
-        pass  # todo Generate a dynamic sized list of tokens given the hidden state
+        """
+        Generates a list of candidate actions from a hidden state by sequentially
+        generating action components until a stop token is produced.
+        """
+        actions = []
+        # Use eval mode for generation to disable dropout etc.
+        self.eval()
+        with torch.no_grad():
+            # Project hidden state to initial LSTM state
+            h = self.hidden_to_lstm_h(hidden_state)
+            c = self.hidden_to_lstm_c(hidden_state)
 
+            for _ in range(num_actions_to_sample):
+                action_components = []
+                # Start with the learnable start-of-action token
+                input_token_emb = self.start_action_token
+                current_h, current_c = h.clone(), c.clone()
+
+                for comp_name in self.action_component_names:
+                    current_h, current_c = self.action_decoder_lstm(
+                        input_token_emb, (current_h, current_c)
+                    )
+
+                    head = self.action_component_heads[comp_name]
+                    logits = head(current_h)
+
+                    # Sample from the distribution. Could use argmax for deterministic generation.
+                    dist = torch.distributions.Categorical(logits=logits)
+                    sampled_token_idx = dist.sample()
+
+                    cardinality = self.network_spec["cardinalities"][comp_name]
+                    # Values are 1..cardinality+1. 0 is pad. stop is cardinality+2
+                    stop_token_idx = cardinality + 2
+
+                    if sampled_token_idx == stop_token_idx:
+                        break  # End of action
+
+                    # Convert from vocabulary index back to original component value
+                    # Vocab index is val + 1. So val is index - 1.
+                    component_value = sampled_token_idx.item() - 1
+                    action_components.append(component_value)
+
+                    # Prepare input for the next step
+                    input_token_emb = self.embedding_layers[comp_name](
+                        sampled_token_idx
+                    )
+
+                if action_components:
+                    # Format action based on number of components
+                    if len(action_components) == 1:
+                        actions.append(action_components[0])
+                    else:
+                        actions.append(tuple(action_components))
+
+        return list(set(actions))  # Return unique actions generated
+
+    # action torch.Tensor, not action type. It is an *encoded* action.
     def get_policy_and_value(
         self, hidden_state: torch.Tensor, candidate_actions: List[torch.Tensor]
     ) -> Tuple[Dict[ActionType, float], float]:
