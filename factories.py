afe17ce0a13edfe7b6e96ea5c07a831f8fe0fd98
Toren Darby <torendarby@gmail.com>
2025-09-08 20:08:12 -0700

    add muzero to factories

--- factories.py
+++ factories.py
@@ -10,6 +10,8 @@ from agents.alphazero.alphazero_agent import (
     AlphaZeroEvaluation,
 )
 from agents.alphazero.alphazero_net import AlphaZeroNet
+from agents.base_learning_agent import BaseLearningAgent
+from agents.muzero.muzero_agent import MuZeroAgent, make_pure_muzero
 from core.agent_interface import Agent
 from core.config import (
     AppConfig,
@@ -19,6 +21,49 @@ from environments.base import BaseEnvironment
 from environments.connect4 import Connect4
 from agents.mcts_agent import make_pure_mcts
 from algorithms.mcts import UCB1Selection, StandardBackpropagation
+
+
+def _create_az_agent(env: BaseEnvironment, config: AppConfig) -> AlphaZeroAgent:
+    az_config = config.alphazero
+    training_config = config.training
+    network = AlphaZeroNet(env=env)
+    network.init_zero()
+    optimizer = optim.AdamW(network.parameters(), lr=training_config.learning_rate)
+
+    az_agent = AlphaZeroAgent(
+        selection_strategy=UCB1Selection(exploration_constant=az_config.cpuct),
+        expansion_strategy=AlphaZeroExpansion(network=network),
+        evaluation_strategy=AlphaZeroEvaluation(network=network),
+        backpropagation_strategy=StandardBackpropagation(),
+        network=network,
+        optimizer=optimizer,
+        env=env,
+        config=az_config,
+        training_config=training_config,
+    )
+    return az_agent
+
+
+def _create_mz_agent(env: BaseEnvironment, config: AppConfig) -> MuZeroAgent:
+    mz_config = config.muzero
+    training_config = config.training
+    mz_agent = make_pure_muzero(
+        env=env,
+        config=mz_config,
+        training_config=training_config,
+    )
+    return mz_agent
+
+
+def _load_and_prepare_agent(agent: BaseLearningAgent, agent_name_base: str):
+    if not agent.load():
+        logger.warning(
+            f"Could not load pre-trained {agent_name_base} weights. Agent will play randomly/poorly."
+        )
+    else:
+        logger.info(f"Loaded pre-trained {agent_name_base} agent.")
+    if agent.network:
+        agent.network.eval()
 
 
 def get_environment(env_config: EnvConfig) -> BaseEnvironment:
@@ -32,52 +77,25 @@ def get_environment(env_config: EnvConfig) -> BaseEnvironment:
 
 def get_agents(env: BaseEnvironment, config: AppConfig) -> Dict[str, Agent]:
     """Factory function to create agent instances for the given environment."""
-    az_config = config.alphazero
-    training_config = config.training
-
     agents = {}
-    network = AlphaZeroNet(env=env)
-    network.init_zero()
-    optimizer = optim.AdamW(network.parameters(), lr=training_config.learning_rate)
 
+    # AlphaZero agent
     az_agent_name = f"AZ_{config.mcts.num_simulations}"
-    az_agent = AlphaZeroAgent(
-        selection_strategy=UCB1Selection(exploration_constant=az_config.cpuct),
-        expansion_strategy=AlphaZeroExpansion(network=network),
-        evaluation_strategy=AlphaZeroEvaluation(network=network),
-        backpropagation_strategy=StandardBackpropagation(),
-        network=network,
-        optimizer=optimizer,
-        env=env,
-        config=az_config,
-        training_config=training_config,
-    )
-    if not az_agent.load():
-        logger.warning(
-            "Could not load pre-trained AlphaZero weights. Agent will play randomly/poorly."
-        )
-    else:
-        logger.info("Loaded pre-trained AlphaZero agent.")
-    if az_agent.network:
-        az_agent.network.eval()
+    az_agent = _create_az_agent(env, config)
+    _load_and_prepare_agent(az_agent, "AlphaZero")
     agents[az_agent_name] = az_agent
 
+    # MuZero agent
+    mz_agent_name = f"MZ_{config.mcts.num_simulations}"
+    mz_agent = _create_mz_agent(env, config)
+    _load_and_prepare_agent(mz_agent, "MuZero")
+    agents[mz_agent_name] = mz_agent
+
+    # MCTS agent
     mcts_agent_name = f"MCTS_{config.mcts.num_simulations}"
     mcts_agent = make_pure_mcts(
         num_simulations=config.mcts.num_simulations,
     )
     agents[mcts_agent_name] = mcts_agent
 
-    # agents["Random"] = RandomAgent(env=env)
-
-    # mcts_agent_old_name = f"MCTS_old_{config.mcts.num_simulations}"
-    # mcts_agent_old = MCTSAgent_Old(
-    #     env=env,
-    #     num_simulations=config.mcts.num_simulations,
-    #     exploration_constant=config.mcts.exploration_constant,
-    # )
-
     return agents
-
-
-# TODO: Add factory for MuZero agent when implemented
