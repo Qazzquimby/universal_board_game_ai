d857ab784ea609275a356a800552a6242f168c76
Toren Darby <torendarby@gmail.com>
2025-10-29 10:43:24 -0700

    add player reversed sanity tests

--- environments/connect4.py
+++ environments/connect4.py
@@ -306,9 +306,54 @@ class Connect4(BaseEnvironment):
             )
         )
 
-        # --- State 3: Player 1 can win vertically in column 0 ---
+        # 2.5, player swapped state 2
+        env2_5 = Connect4()
+        env2_5.state["pieces"] = DataFrame(
+            [
+                (5, 0, 1),
+                (5, 1, 1),
+                (5, 2, 1),
+                (5, 4, 0),
+                (5, 5, 0),
+            ],
+            columns=["row", "col", "player_id"],
+        )
+        env2_5.state["game"] = env2_5.state["game"].with_columns({"current_player": 1})
+        states.append(
+            SanityCheckState(
+                description="Player 1 can win horizontally (col 3)",
+                state_with_key=env2_5.get_state_with_key(),
+                expected_value=1.0,
+                expected_action=3,
+            )
+        )
+
+        # --- State 3: Player 0 can win vertically in column 0 ---
         env3 = Connect4()
         env3.state["pieces"] = DataFrame(
+            [
+                (5, 0, 1),
+                (4, 2, 1),
+                (5, 2, 1),
+                (3, 2, 1),
+                (4, 0, 0),
+                (3, 0, 0),
+                (2, 0, 0),
+            ],
+            columns=["row", "col", "player_id"],
+        )
+        states.append(
+            SanityCheckState(
+                description="Player 0 can win vertically (col 0)",
+                state_with_key=env3.get_state_with_key(),
+                expected_value=1.0,
+                expected_action=0,
+            )
+        )
+
+        # --- State 3.5: Player 1 can win vertically in column 0 ---
+        env3_5 = Connect4()
+        env3_5.state["pieces"] = DataFrame(
             [
                 (5, 0, 0),
                 (4, 2, 0),
@@ -320,11 +365,11 @@ class Connect4(BaseEnvironment):
             ],
             columns=["row", "col", "player_id"],
         )
-        env3.state["game"] = env3.state["game"].with_columns({"current_player": 1})
+        env3_5.state["game"] = env3_5.state["game"].with_columns({"current_player": 1})
         states.append(
             SanityCheckState(
                 description="Player 1 can win vertically (col 0)",
-                state_with_key=env3.get_state_with_key(),
+                state_with_key=env3_5.get_state_with_key(),
                 expected_value=1.0,
                 expected_action=0,
             )
@@ -348,6 +393,29 @@ class Connect4(BaseEnvironment):
             SanityCheckState(
                 description="Player 0 must block P1 win (col 6)",
                 state_with_key=env4.get_state_with_key(),
+                expected_action=6,
+            )
+        )
+
+        # --- State 4.5: Player 1 must block Player 0's win in column 6 ---
+        env4_5 = Connect4()
+        env4_5.state["pieces"] = DataFrame(
+            [
+                (5, 0, 1),
+                (4, 0, 1),
+                (5, 1, 1),
+                (4, 1, 1),
+                (5, 6, 0),
+                (4, 6, 0),
+                (3, 6, 0),
+            ],
+            columns=["row", "col", "player_id"],
+        )
+        env4_5.state["game"] = env4_5.state["game"].with_columns({"current_player": 1})
+        states.append(
+            SanityCheckState(
+                description="Player 1 must block P1 win (col 6)",
+                state_with_key=env4_5.get_state_with_key(),
                 expected_action=6,
             )
         )
