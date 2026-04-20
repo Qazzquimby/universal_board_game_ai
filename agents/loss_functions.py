8e8709416d0cd34048b6ea00d99cb2980d144c4f
Toren Darby <torendarby@gmail.com>
2026-04-20 16:22:24 -0700

    Use nan safe cross entropy

--- agents/loss_functions.py
+++ agents/loss_functions.py
@@ -10,19 +10,13 @@ def entropy_adjusted_cross_entropy_loss(
     This makes the loss tend to 0 for a perfect prediction.
     """
     log_probs = F.log_softmax(logits, dim=1)
-    return F.kl_div(log_probs, targets, reduction="batchmean")
 
-    # log_probs = F.log_softmax(
-    #     logits, dim=1
-    # )  # missing .exp()? # should be using kl_div?
-    # cross_entropy_term = targets * log_probs
-    # cross_entropy_term = torch.nan_to_num(cross_entropy_term, nan=0.0)
-    # cross_entropy = -torch.sum(cross_entropy_term, dim=1).mean()
-    #
-    # eps = 1e-9
-    # with torch.no_grad():
-    #     target_entropy_term = targets * torch.log(targets + eps)
-    #     target_entropy_term = torch.nan_to_num(target_entropy_term, nan=0.0)
-    #     target_entropy = -torch.sum(target_entropy_term, dim=1).mean()
-    #
-    # return cross_entropy - target_entropy
+    # 1. Substitute -inf with 0.0 in log_probs where target is 0 to preven nan
+    safe_log_probs = torch.where(targets > 0, log_probs, torch.zeros_like(log_probs))
+    cross_entropy_term = targets * safe_log_probs
+    cross_entropy = -torch.sum(cross_entropy_term, dim=1).mean()
+
+    with torch.no_grad():
+        target_entropy = torch.sum(torch.special.entr(targets), dim=1).mean()
+
+    return cross_entropy - target_entropy
